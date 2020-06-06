444c922ef3321b83170343ee4bddfb948b6d2852
Sadettin Ayyıldız <53340224+SadettinAyyildiz@users.noreply.github.com>
2020-06-06 18:15:09 +0300

    Activity #1 Completed

---
+++ Part4PracticeActivity1/src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String gunlukAntrenman() {
+		// TODO Auto-generated method stub
+		return "30 dk toplu calisma yapti";
+	}
+
+}
---
+++ Part4PracticeActivity1/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String gunlukAntrenman() {
+		// TODO Auto-generated method stub
+		return "45 dakika tenis oynadi";
+	}
+
+}
